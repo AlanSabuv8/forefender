30c6e3173133930317439982822a8eb5aae42719
AlanSabuv8 <alansabuv8@gmail.com>
2024-02-02 10:48:44 +0530

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import LoginSignUp from './Components/LS/LoginSignUp.jsx';
+
+function App() {
+
+  return (
+    <div>
+      <LoginSignUp/>
+    </div>
+  );
+}
+
+export default App
